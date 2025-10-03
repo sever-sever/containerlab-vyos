ba4aebe010a2aa8b6ae531a8b63fc0f8d2caaf94
Viacheslav <v.gletenko@vyos.io>
2025-10-03 02:33:18 +0000

    Add terraform config for VyOS nodes

--- main.tf
+++ main.tf
@@ -2,18 +2,18 @@ locals {
   routers = {
     r1 = {
       address = "192.0.2.21"
-      cfg     = "config-r1.cfg"
+      cfg     = "containerlab/config/config-r1.cfg"
     }
     r2 = {
       address = "192.0.2.22"
-      cfg     = "config-r2.cfg"
+      cfg     = "containerlab/config/config-r2.cfg"
     }
   }
 }
 
 module "router_config" {
-  source        = "github.com/apnex/mod-vyos-config"
-  for_each      = local.routers
+  source   = "github.com/apnex/mod-vyos-config"
+  for_each = local.routers
 
   mgmt_address  = each.value.address
   mgmt_username = "admin"
